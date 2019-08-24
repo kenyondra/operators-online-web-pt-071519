e37676424d2c06eeb85012b88a76b151563c0b65
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-08-24 18:46:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def unsafe?(speed)
-if speed > 60 || < 40
-  return true; 
-else 
-  return false; 
+  if speed > 60 || < 40
+    return true; 
+  else 
+    return false; 
 end
 
 
